5698313f7635e27304ac24a2872e7f5e8e27b0cf
Alejandro Merino <alejandro2hpxa@gmail.com>
2019-07-03 17:48:23 -0500

    bonito

--- src/exchangeHouse/TransactionList.js
+++ src/exchangeHouse/TransactionList.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 
+const getBeautyDate = (date) => {
+    let bDate = Date.parse(date);
+    let dateString =  new Date(bDate);
+    return dateString.toLocaleString();
+}
+
 export class TransactionList extends Component {
 
     render() {
@@ -10,13 +16,13 @@ export class TransactionList extends Component {
         return this.props.transactions.map( transaction => 
             <div className="card m-1 p-1 bg-light" key={transaction.idTransaction}>
                 <div>
-                    {transaction.transactionDate}
+                    {getBeautyDate(transaction.transactionDate)}
                 </div>
                 <div>
                     {transaction.clientName + ": " + transaction.clientDocumentType + " " + transaction.clientDocumentNumber}
                 </div>
                 <div>
-                    {transaction.mountInt + " A " + transaction.mountOut}
+                    {transaction.mountInt.substring(0,8) + "  ==>>  " + transaction.mountOut.substring(0,8)}
                 </div>
                 <span></span>
             </div>
